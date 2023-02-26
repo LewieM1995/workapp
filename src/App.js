9251b1cb91c083c2efcdb517fae0c3fb3eb1b3ff
LewieM1995 <Lewiemarks@gmail.com>
2023-02-26 19:39:02 +0000

    redirect testing

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
 
 
   return (
-    <Router basename="/">
+    <Router basename="/batchpost">
       <Main />
         <Routes>
           <Route exact path="/" element={<SearchPage/>}/>
